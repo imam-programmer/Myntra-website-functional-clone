948dd7947434ea5ced42327664fe2c37da9e1692
imam-programmer <imamprogrammer09@gmail.com>
2026-06-12 08:29:29 +0600

    selecting product deleted functionality done

--- scripts/bag.js
+++ scripts/bag.js
@@ -1,6 +1,6 @@
 let selectingProducts=document.querySelector('.bag-items-container');
-// selectingProducts.innerHTML=
 
+let item=0;
 let selectProductobj;
 selectProductobj=bagadd_array.map((e)=>{
     for(let i=0;i<items.length; i++){
@@ -9,6 +9,43 @@ selectProductobj=bagadd_array.map((e)=>{
         }
     }
 })
+
+function itemDelet(item){
+bagadd_array=bagadd_array.filter(e=>e!=item)
+localStorage.setItem("products",JSON.stringify(bagadd_array))
+setstorage()
+selectProductobj=bagadd_array.map((e)=>{
+    for(let i=0;i<items.length; i++){
+        if(e==items[i].id){
+            return items[i]
+        }
+    }
+  
+})
+
+finalBaypart.innerHTML=` <div class="bag-details-container">
+            <div class="price-header">PRICE DETAILS (${item=bagadd_array.length} Items) </div>
+            <div class="price-item">
+              <span class="price-item-tag">Total MRP</span>
+              <span class="price-item-value">Rs </span>
+            </div>
+            <div class="price-item">
+              <span class="price-item-tag">Discount on MRP</span>
+              <span class="price-item-value priceDetail-base-discount">-Rs143</span>
+            </div>
+            <div class="price-item">
+              <span class="price-item-tag">Convenience Fee</span>
+              <span class="price-item-value">Rs 99</span>
+            </div>
+            <hr>
+            <div class="price-footer">
+              <span class="price-item-tag">Total Amount</span>
+              <span class="price-item-value">Rs 1240</span>
+            </div>
+          </div>`
+
+
+
 
 let innerHTML=""
 selectProductobj.map((e)=>{
@@ -33,19 +70,53 @@ selectProductobj.map((e)=>{
               </div>
             </div>
 
-            <div class="remove-from-cart" onclick="itemDelet()">X</div>
+            <div class="remove-from-cart" onclick="itemDelet(${e.id})">X</div>
+          </div>`
+})
+selectingProducts.innerHTML=innerHTML;
+
+}
+
+
+let innerHTML=""
+selectProductobj.map((e)=>{
+    return innerHTML+=`<div class="bag-item-container">
+            <div class="item-left-part">
+              <img class="bag-item-img" src="../${e.image}">
+            </div>
+            <div class="item-right-part">
+              <div class="company">${e.company}</div>
+              <div class="item-name">${e.item_name}</div>
+              <div class="price-container">
+                <span class="current-price">Rs ${e.current_price}</span>
+                <span class="original-price">Rs ${e.original_price}</span>
+                <span class="discount-percentage">(${e.discount_percentage}% OFF)</span>
+              </div>
+              <div class="return-period">
+                <span class="return-period-days">${e.return_period} days</span> return available
+              </div>
+              <div class="delivery-details">
+                Delivery by
+                <span class="delivery-details-days">${e.delivery_date}</span>
+              </div>
+            </div>
+
+            <div class="remove-from-cart" onclick="itemDelet(${e.id})">X</div>
           </div>`
 })
 selectingProducts.innerHTML=innerHTML;
 
 let finalBaypart=document.querySelector(".bag-summary");
 
-let item=0;
+
+let mrp=0;
+let discount_mrp=0;
+let Convenience=0;
 finalBaypart.innerHTML=` <div class="bag-details-container">
-            <div class="price-header">PRICE DETAILS (2 Items) </div>
+            <div class="price-header">PRICE DETAILS (${item=bagadd_array.length} Items) </div>
             <div class="price-item">
               <span class="price-item-tag">Total MRP</span>
-              <span class="price-item-value">Rs1284</span>
+              <span class="price-item-value">Rs </span>
             </div>
             <div class="price-item">
               <span class="price-item-tag">Discount on MRP</span>
